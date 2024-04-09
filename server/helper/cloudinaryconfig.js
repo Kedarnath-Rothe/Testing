e1104f7ee1419cd2c452075d6b4e2e1ba8d9f137
Kedarnath <kedarrothe05@gmail.com>
2024-04-10 00:44:12 +0530

    First

---
+++ server/helper/cloudinaryconfig.js
@@ -0,0 +1,10 @@
+const cloudinary = require("cloudinary").v2;
+
+
+cloudinary.config({
+    cloud_name:'diyw5ilre',
+    api_key:'254567682481541',
+    api_secret:'SZjLHZQnlbVKZfsInZYeLnBakIM'
+});
+
+module.exports = cloudinary
